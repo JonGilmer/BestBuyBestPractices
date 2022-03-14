1ed4dcdd8d4d1215c36596818cb29d171c4dbfc2
Jonathan Gilmer <jonathan@jonathangilmer.com>
2022-03-14 15:30:52 -0400

    setup Dapper functionality

---
+++ BestBuyBestPractices/DapperDepartmentsRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Dapper;
+
+namespace BestBuyBestPractices
+{
+    public class DapperDepartmentsRepository : IDepartmentsRepository
+    {
+        private readonly IDbConnection _conn;
+
+        public DapperDepartmentsRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        public IEnumerable<departments> GetAllDepartments()
+        {
+            return _conn.Query<departments>("SELECT * FROM departments;");
+        }
+    }
+}
