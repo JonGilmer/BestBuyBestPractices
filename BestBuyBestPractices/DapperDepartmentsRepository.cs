906cdf9da8f53508990cc42b040737c5384409b6
Jonathan Gilmer <jonathan@jonathangilmer.com>
2022-03-14 16:55:35 -0400

    Part 2 of exercise in progress

--- BestBuyBestPractices/DapperDepartmentsRepository.cs
+++ BestBuyBestPractices/DapperDepartmentsRepository.cs
@@ -7,16 +7,22 @@ namespace BestBuyBestPractices
 {
     public class DapperDepartmentsRepository : IDepartmentsRepository
     {
-        private readonly IDbConnection _conn;
+        private readonly IDbConnection _connection;
 
-        public DapperDepartmentsRepository(IDbConnection conn)
+        public DapperDepartmentsRepository(IDbConnection connection)
         {
-            _conn = conn;
+            _connection = connection;
         }
 
         public IEnumerable<departments> GetAllDepartments()
         {
-            return _conn.Query<departments>("SELECT * FROM departments;");
+            return _connection.Query<departments>("SELECT * FROM departments;");
+        }
+
+        public void InsertDepartment(string newDepartmentName)
+        {
+            _connection.Execute("INSERT INTO DEPARTMENTS (NAME) VALUES (@departmentName);",
+                new { departmentName = newDepartmentName });
         }
     }
 }
--- BestBuyBestPractices/Program.cs
+++ BestBuyBestPractices/Program.cs
@@ -18,7 +18,30 @@ namespace BestBuyBestPractices
                             .Build();
 
             string connString = config.GetConnectionString("DefaultConnection");
+
             IDbConnection conn = new MySqlConnection(connString);
+
+                // instantiate new DapperDepartmentsRepository
+            var repo = new DapperDepartmentsRepository(conn);
+
+            Console.WriteLine("Type a new Department name: ");
+
+            var newDepartment = Console.ReadLine();
+            Console.WriteLine("");
+
+            repo.InsertDepartment(newDepartment);
+
+                // calls the method and returns dept info from database in the form of an IEnumerable
+            var departments = repo.GetAllDepartments();
+
+            foreach (var dept in departments)
+            {
+                Console.WriteLine($"{dept.DepartmentID} {dept.Name}");
+            }
+
+
+
+            var repo = new DapperProductRepository(connection);
         }
     }
 }
